087614ac6e158b97836524a1e8af41ce7f9e8d85
suojae <suojae3@gmail.com>
2023-07-16 21:37:19 +0900

    initial commit

---
+++ LiveForToday/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  LiveForToday
+//
+//  Created by ㅣ on 2023/07/15.
+//
+
+import UIKit
+
+
+class LoginViewController: UIViewController {
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // navigation bar 숨김
+        navigationController?.navigationBar.isHidden = false
+    }
+    
+    
+    
+}
